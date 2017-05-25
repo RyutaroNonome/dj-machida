0a18957cbee25503930da792de33a4e10afdbfbb
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2017-05-25 10:44:38 +0900

    @wp → wp

--- app.rb
+++ app.rb
@@ -32,23 +32,23 @@ require 'oauth2'
     auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => @refresh_token, :expires_at => 3600})
     auth_token = auth_token.refresh!
     session = GoogleDrive.login_with_oauth(auth_token.token)
-    @ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
+    ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
     # Spread.new()
     # B, C, D列にスラックから記入
     @row_id , @col_id = 0
 
     # レコード数を取得
-    p @ws.num_rows
+    p ws.num_rows
     # カラム数を取得
-    p @ws.num_cols
+    p ws.num_cols
     @row_id = ws.num_rows
-    @ws[@row_id + 1, 2] = @url
-    @ws[@row_id + 1, 3] = @description
+    ws[@row_id + 1, 2] = @url
+    ws[@row_id + 1, 3] = @description
     # ws[@row_id + 1, 4] = user_name
     @ws[@row_id + 1, 5] = @user
 
 
-    @ws.save
+    ws.save
     #id リセット
     @row_id , @column_id = 0
   end
@@ -63,13 +63,13 @@ require 'oauth2'
     auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => @refresh_token, :expires_at => 3600})
     auth_token = auth_token.refresh!
     session = GoogleDrive.login_with_oauth(auth_token.token)
-    @ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
+    ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
 
     # 2行目以降のランダムな数字を出力 0.1.2... → 2.3.4...
     # Spread.new()
-    random_num = rand(@ws.num_rows.to_i) + 2
-    @getted_description = @ws[random_num, 3]
-    @getted_url = @ws[random_num, 2]
+    random_num = rand(ws.num_rows.to_i) + 2
+    @getted_description = ws[random_num, 3]
+    @getted_url = ws[random_num, 2]
     return @getted_url + "\n" + @getted_description
     # puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     # return @ws[random_num, 3] + "\n" + @ws[random_num, 2]
