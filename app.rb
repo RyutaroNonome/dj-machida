6c4aab6215657e54cc33a6043283250f7a1afcfe
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2017-05-25 10:21:53 +0900

    class method to instance method

--- app.rb
+++ app.rb
@@ -9,7 +9,20 @@ class Spread
   @refresh_token       = ENV['refresh_token']
   @MY_SPREAD_SHEET_KEY = ENV['MY_SPREAD_SHEET_KEY']
 
-  def initialize()
+  # def initialize()
+  #   client = OAuth2::Client.new(
+  #     @client_id,
+  #     @client_secret,
+  #     site: "https://accounts.google.com",
+  #     token_url: "/o/oauth2/token",
+  #     authorize_url: "/o/oauth2/auth")
+  #   auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => @refresh_token, :expires_at => 3600})
+  #   auth_token = auth_token.refresh!
+  #   session = GoogleDrive.login_with_oauth(auth_token.token)
+  #   @ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
+  # end
+
+  def self.access_spreadsheet
     client = OAuth2::Client.new(
       @client_id,
       @client_secret,
@@ -20,20 +33,7 @@ class Spread
     auth_token = auth_token.refresh!
     session = GoogleDrive.login_with_oauth(auth_token.token)
     @ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
-  end
-
-  def self.access_spreadsheet
-    # client = OAuth2::Client.new(
-    #   @client_id,
-    #   @client_secret,
-    #   site: "https://accounts.google.com",
-    #   token_url: "/o/oauth2/token",
-    #   authorize_url: "/o/oauth2/auth")
-    # auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => @refresh_token, :expires_at => 3600})
-    # auth_token = auth_token.refresh!
-    # session = GoogleDrive.login_with_oauth(auth_token.token)
-    # ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
-    Spread.new()
+    # Spread.new()
     # B, C, D列にスラックから記入
     @row_id , @col_id = 0
 
@@ -54,25 +54,24 @@ class Spread
   end
 
   def self.get_url_by_spreadsheet
-    # client = OAuth2::Client.new(
-    #   @client_id,
-    #   @client_secret,
-    #   site: "https://accounts.google.com",
-    #   token_url: "/o/oauth2/token",
-    #   authorize_url: "/o/oauth2/auth")
-    # auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => @refresh_token, :expires_at => 3600})
-    # auth_token = auth_token.refresh!
-    # session = GoogleDrive.login_with_oauth(auth_token.token)
-    # ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
+    client = OAuth2::Client.new(
+      @client_id,
+      @client_secret,
+      site: "https://accounts.google.com",
+      token_url: "/o/oauth2/token",
+      authorize_url: "/o/oauth2/auth")
+    auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => @refresh_token, :expires_at => 3600})
+    auth_token = auth_token.refresh!
+    session = GoogleDrive.login_with_oauth(auth_token.token)
+    @ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
 
     # 2行目以降のランダムな数字を出力 0.1.2... → 2.3.4...
     # Spread.new()
-    # random_num = rand(@ws.num_rows.to_i) + 2
-    # # @getted_description = @ws[random_num, 3]
-    # # @getted_url = @ws[random_num, 2]
-    # # return @getted_url + "\n" + @getted_description
-    # puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-
-    # return @ws[random_num, 3] + "\n" + @ws[random_num, 2]
+    random_num = rand(@ws.num_rows.to_i) + 2
+    # @getted_description = @ws[random_num, 3]
+    # @getted_url = @ws[random_num, 2]
+    # return @getted_url + "\n" + @getted_description
+    puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    return @ws[random_num, 3] + "\n" + @ws[random_num, 2]
   end
 end
