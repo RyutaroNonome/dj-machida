1a264dc3d4aceaadfe87eea384e1afedfb6f349c
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2017-06-01 16:02:02 +0900

    modified

--- app.rb
+++ app.rb
@@ -3,26 +3,11 @@ require "google_drive"
 require "pp"
 require 'oauth2'
 
-# class Spread
   @client_id           = ENV['client_id']
   @client_secret       = ENV['client_secret']
   @refresh_token       = ENV['refresh_token']
   @MY_SPREAD_SHEET_KEY = ENV['MY_SPREAD_SHEET_KEY']
-
-  # def initialize()
-  #   client = OAuth2::Client.new(
-  #     @client_id,
-  #     @client_secret,
-  #     site: "https://accounts.google.com",
-  #     token_url: "/o/oauth2/token",
-  #     authorize_url: "/o/oauth2/auth")
-  #   auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => @refresh_token, :expires_at => 3600})
-  #   auth_token = auth_token.refresh!
-  #   session = GoogleDrive.login_with_oauth(auth_token.token)
-  #   @ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
-  # end
-
-  def access_spreadsheet
+  def init
     client = OAuth2::Client.new(
       @client_id,
       @client_secret,
@@ -33,10 +18,11 @@ require 'oauth2'
     auth_token = auth_token.refresh!
     session = GoogleDrive.login_with_oauth(auth_token.token)
     ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
-    # Spread.new()
-    # B, C, D列にスラックから記入
-    @row_id , @col_id = 0
+  end
 
+  def access_spreadsheet
+    ws = init
+    @row_id , @col_id = 0
     # レコード数を取得
     p ws.num_rows
     # カラム数を取得
@@ -44,7 +30,6 @@ require 'oauth2'
     @row_id = ws.num_rows
     ws[@row_id + 1, 2] = @url
     ws[@row_id + 1, 3] = @description
-    # ws[@row_id + 1, 4] = user_name
     ws[@row_id + 1, 5] = @user
 
 
@@ -54,24 +39,10 @@ require 'oauth2'
   end
 
   def get_url_by_spreadsheet
-    client = OAuth2::Client.new(
-      @client_id,
-      @client_secret,
-      site: "https://accounts.google.com",
-      token_url: "/o/oauth2/token",
-      authorize_url: "/o/oauth2/auth")
-    auth_token = OAuth2::AccessToken.from_hash(client,{:refresh_token => @refresh_token, :expires_at => 3600})
-    auth_token = auth_token.refresh!
-    session = GoogleDrive.login_with_oauth(auth_token.token)
-    ws = session.spreadsheet_by_key(@MY_SPREAD_SHEET_KEY).worksheets[0]
-
-    # 2行目以降のランダムな数字を出力 0.1.2... → 2.3.4...
-    # Spread.new()
+    ws = init
+    # 2行目以下のランダムな数字を出力 0.1.2... → 2.3.4...
     random_num = rand(ws.num_rows.to_i) + 2
     @getted_description = ws[random_num, 3]
     @getted_url = ws[random_num, 2]
     return @getted_url + "\n" + @getted_description
-    # puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    # return @ws[random_num, 3] + "\n" + @ws[random_num, 2]
   end
-# end
